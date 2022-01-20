dd69f0eccce12b5fefe71fc0eafa719ff64ff563
andreisfx <sfxandrei@gmail.com>
2022-01-20 16:47:36 +0200

    push

--- src/main/java/CreateTree.java
+++ src/main/java/CreateTree.java
@@ -1,15 +1,27 @@
 public class CreateTree {
     public static void main(String[] args) {
+            int inaltime=20;
+            int tulpina=inaltime/4;
+        for (int i = 0; i < inaltime+tulpina; i++) {
 
-        for (int i = 0; i < 10; i++) {
+            if(i<=inaltime){
+                for (int j = 0; j < inaltime - i; j++){
+                    System.out.print(" ");
+                }
 
-            for (int j = 0; j < 10 - i; j++){
-                System.out.print(" ");
+                for (int k = 0; k < (2 * i + 1); k++){
+                    System.out.print("*");
+                }
+            }else{
+                for(int t=0;t<(inaltime-tulpina);t++){
+                    System.out.print(" ");
+                }
+                for(int tp=(inaltime/2);tp<inaltime;tp++){
+                    System.out.print("*");
+                }
             }
 
-            for (int k = 0; k < (2 * i + 1); k++){
-                System.out.print("*");
-            }
+
 
             System.out.println();
         }
